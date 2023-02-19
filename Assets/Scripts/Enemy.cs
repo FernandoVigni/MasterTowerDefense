431498de06819aeef41027d2bb229e5336c76ef5
Fernando Vigni <95660918+FernandoVigni@users.noreply.github.com>
2023-02-19 01:27:17 -0300

    ok
    okok

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int life;
+
     public float speed = 5f; // velocidad a la que se mueve el enemigo hacia la torre
     private Transform target; // objeto de destino, en este caso la torre
+    public float distanceToTarget; // variable para almacenar la distancia al objetivo
 
     private void Start()
     {
@@ -16,10 +19,16 @@ public class Enemy : MonoBehaviour
     private void Update()
     {
         // Si el objetivo existe, mueve el enemigo hacia él
-        if (target != null)
+        if (target != null && distanceToTarget > 2)
         {
             transform.LookAt(target); // mira hacia el objetivo
             transform.Translate(Vector3.forward * speed * Time.deltaTime); // se mueve hacia el objetivo
         }
+        
+        // Calcula la distancia entre el enemigo y la torre
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+        // Imprime la distancia actual en la consola
+        Debug.Log("Distancia a la torre: " + distanceToTarget);
     }
 }
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    private List<GameObject> enemyList = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +17,52 @@ public class Tower : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            // Agregamos el objeto "Enemy" a la lista si no está ya en ella
+            if (!enemyList.Contains(other.gameObject))
+            {
+                enemyList.Add(other.gameObject);
+            }
+
+            // Si la vida del objeto "Enemy" llega a 0, lo eliminamos de la lista
+            if (other.gameObject.GetComponent<Enemy>().life <= 0)
+            {
+                enemyList.Remove(other.gameObject);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            // Eliminamos el objeto "Enemy" de la lista si está en ella
+            if (enemyList.Contains(other.gameObject))
+            {
+                enemyList.Remove(other.gameObject);
+            }
+        }
+    }
+
+    public GameObject GetNearestEnemy()
+    {
+        // Ordenamos la lista de enemigos por distancia
+        enemyList.Sort((a, b) => Vector3.Distance(a.transform.position, transform.position).CompareTo(Vector3.Distance(b.transform.position, transform.position)));
+
+        // Devolvemos el primer enemigo de la lista, si la lista no está vacía
+        if (enemyList.Count > 0)
+        {
+            return enemyList[0];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
 }
