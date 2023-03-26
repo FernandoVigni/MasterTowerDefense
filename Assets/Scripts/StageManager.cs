c9c0dc561ca349160bc13ab3075781949af7728f
Fernando Vigni <95660918+FernandoVigni@users.noreply.github.com>
2023-03-26 12:16:38 -0300

    Spawn enemies angles working

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class StageManager : MonoBehaviour
 {
     public EnemyManager enemyManager;
     public Transform pointOfSpawnOfWave;
 
-    float[] coefficientsByLevel = { 2f, 1.2f, 1.5f, 1.75f, 2f, 2.5f };
+    float[] coefficientsByLevel = { 1f, 1.2f, 1.5f, 1.75f, 2f, 2.5f };
     int[] amountOfBagOfGoldByLevel = { 1000, 1200, 1300, 2000, 2500 };    
     int[] amountOfWarriosByLevel = { 12, 10, 15, 20, 5 };
     int[] amountOfGiantsByLevel = { 4, 7, 6, 8, 3 };
@@ -21,6 +22,7 @@ public class StageManager : MonoBehaviour
     public int wave;
     public float waveLimitTime;
     public float bagOfGold;
+    public float spawnDistanceToTower;
 
     void Start()
     {
@@ -32,11 +34,6 @@ public class StageManager : MonoBehaviour
     {
         enemyManager = FindObjectOfType<EnemyManager>();
     }
-
-            public float GetCoefficient()
-            {
-                return coefficientsByLevel[currentLevel];
-            }
 
     public void SetLevel(int curentLevel)
     {
@@ -82,12 +79,55 @@ public class StageManager : MonoBehaviour
                 enemyManager.ShufleList(enemyManager.listOfEnemiesToDefeatInThisStage);
                 Enemy enemy = enemyManager.GetFirstEnemyFromStageList();
                 await Task.Delay(1500);
-                enemy.transform.position = pointOfSpawnOfWave.transform.position;
+
+                // 1) random 0 - 360      90
+                int randonAngle = GetRandomNumberBetween360();
+
+                // 2) calculo sin         
+                double x = GetSineOfAnAngle(randonAngle);
+                float floatX = (float)x;
+
+                // 3) calculo cos         
+                double y = GetCosOfAnAngle(randonAngle);
+                float floatY = (float)y;
+
+                Vector3 spawnPositionRandom = new Vector3(floatX, 0, floatY);
+                Debug.Log(spawnPositionRandom);
+
+
+                enemy.transform.position = spawnPositionRandom;
+
+                
                 enemy.LookTower();
                 enemy.StartMove();
                 enemyManager.RemoveEnemyFromStageList(enemy);
                 enemyManager.AddEnemyToSentList(enemy);
             } 
         }   
-    }       
+    }
+
+    public double GetSineOfAnAngle(int angle) 
+    {
+        double angleInRadians = angle * Math.PI / 180;
+        double sineValue = Math.Sin(angleInRadians);
+        sineValue = sineValue * spawnDistanceToTower;
+        sineValue = Math.Round(sineValue, 2);
+        return sineValue;
+    }
+
+    public double GetCosOfAnAngle(int angle)
+    {
+        double angleInRadians = angle * Math.PI / 180;
+        double cosValue = Math.Cos(angleInRadians);
+        cosValue = cosValue * spawnDistanceToTower;
+        cosValue = Math.Round(cosValue, 2);
+        return cosValue;
+    }
+
+    public int GetRandomNumberBetween360()
+    {
+        int randomNumber = UnityEngine.Random.Range(0, 360);
+        return randomNumber;
+    }
+
 }
