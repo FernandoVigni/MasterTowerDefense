2f78e2851d74fddf526972eb1b262541918739ce
Fernando Vigni <95660918+FernandoVigni@users.noreply.github.com>
2023-05-25 15:35:51 -0300

    Audio Manager Working

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,62 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Notifications.iOS;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic("InGameMusic");
+    }
+
+    public void PlayMusic(string name) 
+    {
+        
+       // public static AudioManager Instance;
+
+        Sound sound = Array.Find(musicSounds, x => x.name == name);
+        if (sound == null) 
+        {
+            Debug.Log("Sound not found");
+        }
+        else
+        {
+            musicSource.clip = sound.clip;
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySFX(string name) 
+    {
+        Sound sound = Array.Find(sfxSounds, x => x.name == name);
+        if (sound == null)
+        {
+            Debug.Log("Sound not found");
+        }
+        else
+        {
+            sfxSource.clip = sound.clip;
+            sfxSource.Play();
+        }
+    }
+}
